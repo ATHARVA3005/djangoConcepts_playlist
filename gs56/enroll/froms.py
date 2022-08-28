cec4f2127cbf62fb48c01f89a6c681458c0f56de
ATHARVA3005 <warokaratharva305@gmail.com>
2022-08-28 11:56:52 +0530

    Model Form Inheritance

---
+++ gs56/enroll/froms.py
@@ -0,0 +1,13 @@
+from curses import meta
+from dataclasses import fields
+from django import forms
+from .models import User
+
+class StudentRegistration(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['student_name', 'email', 'password']
+class TeacherRegistration(StudentRegistration):
+    class Meta(StudentRegistration.Meta):
+        fields = ['teacher_name', 'email', 'password']
+
